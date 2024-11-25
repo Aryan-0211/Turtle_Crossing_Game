1a0415d2395cf4c024bb5b7fb57f3fa11cf4a751
Aryan Gadhavi <aryangadhavi79@gmail.com>
2024-11-25 15:43:18 +0530

    Step 4: Created The Scoreboard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
 # Screen setup
 screen = Screen()
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 # Key listener
 screen.listen()
@@ -27,10 +29,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
